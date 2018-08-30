c1afb246776f23de2329d92a341bfa9111362c78
Mario Borras <mario.borras@icloud.com>
2018-08-30 09:55:18 -0400

    changes to the javascript

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -24,17 +24,3 @@ setTimeout(function() {
     document.getElementById("modal").style = "display: none";
   }
   
-  function onClickOpen() {
-    document.getElementById("dropdown").style = "display:block";
-    document.getElementById("close").style = "display:block";
-    document.getElementById("menu-button").style = "display:none";
-    document.getElementById("navCon").style = "background: #555";
-    document.querySelector("body").style = "background: #555";
-  }
-  function onClickClose() {
-    document.getElementById("dropdown").style = "display:none";
-    document.getElementById("close").style = "display:none";
-    document.getElementById("menu-button").style = "display:block";
-    document.querySelector("body").style = "background: white";
-    document.getElementById("navCon").style = "background: white";
-  }
